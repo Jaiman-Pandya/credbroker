d2d04a2e651b6ab37cab815843ed1ba99d407030
Jaiman Pandya <jaimanpandya@gmail.com>
2026-08-22 15:03:48 -0700

    Add the async engine and session factory helpers

---
+++ credbroker/db/session.py
@@ -0,0 +1,16 @@
+"""Async engine and session factory helpers."""
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+
+def create_engine(database_url: str, **kwargs) -> AsyncEngine:
+    return create_async_engine(database_url, pool_pre_ping=True, **kwargs)
+
+
+def create_session_factory(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
+    return async_sessionmaker(engine, expire_on_commit=False)
